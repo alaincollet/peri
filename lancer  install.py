1ae64b40c843cd4cf822956c341f908dd6c4ff73
collet <alain.e.collet@gmail.com>
2024-01-12 10:51:08 +0100

    Add files via upload

---
+++ lancer  install.py
@@ -0,0 +1,113 @@
+# https://hello-k6xnpfueaq-od.a.run.app/   ooo
+
+https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/run/helloworld/main.py
+
+# pip install flask  OOO
+#  gcloud run deploy  dpuis la source
+# gcloud run deploy set --project cfv2024 set run/region europe-west9 set --source "J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld"
+# gcloud run deploy set --project cfv2024 --run/region europe-west9   ooo
+gcloud run deploy set --project cfv2024 --region europe-west9 --source "J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld"
+
+
+Deploying from source. To deploy a container use [--image]. 
+https://cloud.google.com/run/docs/deploying-source-code for more details.
+Source code location (J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld)
+
+ppp gcloud run deploy set --project cfv2024 --region europe-west9 --source "J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld"
+
+
+Deploying from source. To deploy a container use [--image]. See https://cloud.google.com/run/docs/deploying-source-code for more details.
+
+Source code location (J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld):  Y
+gcloud run deploy --source .` to deploy the current directory aaa 
+
+
+
+Building using Buildpacks and deploying container to Cloud Run service [set] in project [cfv2024] region [europe-west9]
+X  Building and deploying new service... Uploading sources.
+  -  Uploading sources...
+  .  Building Container...
+  .  Creating Revision...
+  .  Routing traffic...
+  .  Setting IAM Policy...
+Deployment failed
+ERROR: (gcloud.run.deploy) could not find source [gcloud run deploy set --project cfv2024 --region europe-west9 --source "J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld"]
+
+
+(r-reticulate) J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld>
+
+
+
+(r-reticulate) J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld>gcloud run deploy set --project cfv2024 set run/region europe-west9 set --source "J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld"
+ERROR: (gcloud.run.deploy) unrecognized arguments:
+  set
+  run/region
+  europe-west9
+  To search the help text of gcloud commands, run:
+  gcloud help -- SEARCH_TERMS
+  
+  
+  
+To make this the default region, run `gcloud config set run/region europe-west9`.
+
+This command is equivalent to running `gcloud builds submit --pack image=[IMAGE] y` and `gcloud run deploy set --image [IMAGE]`
+
+Allow unauthenticated invocations to [set] (y/N)?  y
+
+Building using Buildpacks and deploying container to Cloud Run service [set] in project [cfv2024] region [europe-west9]
+X  Building and deploying new service... Uploading sources.
+  -  Uploading sources...
+  .  Building Container...
+  .  Creating Revision...
+  .  Routing traffic...
+  .  Setting IAM Policy...
+Deployment failed
+ERROR: (gcloud.run.deploy) could not find source [y]
+
+
+
+
+
+%%%
+
+(r-reticulate) J:\_DM_PC\_data\_py_scripts\gemini\AS AppsScript PY\cloud service\helloworld>gcloud run deploy set --project cfv2024  set run/region europe-west9 set --source
+ERROR: (gcloud.run.deploy) argument --source: expected one argument
+Usage: gcloud run deploy [[SERVICE] --namespace=NAMESPACE] [optional flags]
+  optional flags may be  --add-cloudsql-instances | --add-custom-audiences |
+                         --allow-unauthenticated | --args | --async |
+                         --binary-authorization | --breakglass |
+                         --clear-binary-authorization |
+                         --clear-cloudsql-instances | --clear-config-maps |
+                         --clear-custom-audiences |
+                         --clear-encryption-key-shutdown-hours |
+                         --clear-env-vars | --clear-key | --clear-labels |
+                         --clear-post-key-revocation-action-type |
+                         --clear-secrets | --clear-vpc-connector | --cluster |
+                         --cluster-location | --command | --concurrency |
+                         --connectivity | --context | --cpu | --cpu-boost |
+                         --cpu-throttling | --description |
+                         --encryption-key-shutdown-hours | --env-vars-file |
+                         --execution-environment | --help | --image |
+                         --ingress | --key | --kubeconfig | --labels |
+                         --max-instances | --memory | --min-instances |
+                         --namespace | --platform | --port |
+                         --post-key-revocation-action-type | --region |
+                         --remove-cloudsql-instances | --remove-config-maps |
+                         --remove-custom-audiences | --remove-env-vars |
+                         --remove-labels | --remove-secrets |
+                         --revision-suffix | --service-account |
+                         --session-affinity | --set-cloudsql-instances |
+                         --set-config-maps | --set-custom-audiences |
+                         --set-env-vars | --set-secrets | --source | --tag |
+                         --timeout | --no-traffic | --update-config-maps |
+                         --update-env-vars | --update-labels |
+                         --update-secrets | --use-http2 | --vpc-connector |
+                         --vpc-egress
+                         
+                         
+                         37] us-west2
+ [38] us-west3
+ [39] us-west4
+ [40] cancel
+Please enter numeric choice or text value (must exactly match list item):  23
+
